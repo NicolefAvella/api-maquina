43ae934b131e4d86284901bf66f0856f90cae6b2
NicolefAvella <nicolef.avella@uptc.edu.co>
2019-07-28 20:30:48 -0500

    test admin y admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _    #funcion obtencion de cadenas texto
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),   # none=titulo seccion
+        (_('Informacion personal'), {'fields': ('name',)}),
+        (
+            _('Permisos'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        #seccion fechas importantes, tiene un campo: ultimo login
+        (_('Fechas importantes'), {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {                                           # none=titulo
+            'classes': ('wide',),                           # valor predeterminado
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse # genera url para pagina admin
+from django.test import Client # permite realizar solicitudes prueba
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='password123',
+            name='Test User Full Name',
+        )
+
+    def test_users_listed(self):
+        """Test usuario esta listado en pagina usuario"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)   # realiza http get en la url
+
+        self.assertContains(res, self.user.name) # se verifica si en res viene el name
+        self.assertContains(res, self.user.email)
+
+    def test_user_page_change(self):
+        """Test pagina de edicion del usuario funciona"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200) # http 200 se refiere ok
+
+    def test_create_user_page(self):
+        """Crear pagina para login"""
+        url = reverse('admin:core_user_add')
+        res = self.client.ger(url)
+
+        self.assertEqual(res.status_code,200)    
